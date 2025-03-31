44fb3d1dfe9a0ff5babbb4072766d33585333ab8
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-31 12:54:20 +0530

    User Authorization Form (#2)
    * setup register/login page
    
    * setup login/register frontend routes
    
    * remove login & register btns

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { createContext, useState } from "react";
 import Navbar from "./components/navbar.component";
+import { Route, Routes } from "react-router-dom";
+import UserAuthForm from "./pages/userAuthForm.page";
 
 export const UserContext = createContext({});
 
@@ -10,7 +12,20 @@ function App() {
   return (
     <UserContext.Provider value={{ userAuth, setUserAuth }}>
       <div className="flex flex-col min-h-screen">
-        <Navbar />
+        {userAuth.access_token && <Navbar />}
+
+        <div className="flex-1">
+          <Routes>
+            {
+              userAuth.access_token ? (
+                <Route path="/" element={<h1>Home page</h1>} />
+              ) : (
+                <Route path="/" element={<UserAuthForm type="login" />} />
+              )
+            }
+            <Route path="/register" element={<UserAuthForm type="register" />}></Route>
+          </Routes>
+        </div>
       </div>
     </UserContext.Provider>
   )
--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -4,33 +4,21 @@ import { UserContext } from '../App';
 
 const Navbar = () => {
 
-    const { userAuth, userAuth: { access_token, profile_img, role } } = useContext(UserContext);
+    const { userAuth: { profile_img, role } } = useContext(UserContext);
 
     return (
         <>
             <nav className="navbar">
                 <Link to='/' className='flex-none w-10'>
-                    <i className="fi fi-rr-user text-3xl text-black"></i>
+                    <i className={"fi fi-rr-" + (role ? 'admin' : 'user') + " text-3xl text-black"}></i>
                 </Link>
 
                 <div className='flex items-center gap-3 md:gap-6 ml-auto'>
-                    {
-                        access_token ?
-                            <div className='relative'>
-                                <button className='w-12 h-12 mt-1'>
-                                    <img src={profile_img} alt="" className='w-full h-full object-cover rounded-full' />
-                                </button>
-                            </div>
-                            :
-                            <>
-                                <Link className='bg-black text-white py-2 px-5 rounded-full hover:bg-gray-800 transition' to='/login'>
-                                    Login
-                                </Link>
-                                <Link className='bg-gray-200 text-gray-800 py-2 px-5 rounded-full hidden md:block hover:bg-gray-300 transition' to='/register'>
-                                    Register
-                                </Link>
-                            </>
-                    }
+                    <div className='relative'>
+                        <button className='w-12 h-12 mt-1'>
+                            <img src={profile_img} alt="" className='w-full h-full object-cover rounded-full' />
+                        </button>
+                    </div>
                 </div>
             </nav>
 
