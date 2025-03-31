0ff3f63e997486958adbf904c2a306aa20c749e0
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-31 12:17:59 +0530

    navbar implementation (#1)
    * setup: tailwindcss v4
    
    * implement navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { createContext, useState } from "react";
+import Navbar from "./components/navbar.component";
+
+export const UserContext = createContext({});
+
 function App() {
 
+  const [userAuth, setUserAuth] = useState({});
+
   return (
-    <>
-      Hello World!
-    </>
+    <UserContext.Provider value={{ userAuth, setUserAuth }}>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+      </div>
+    </UserContext.Provider>
   )
 }
 
