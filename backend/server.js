735b624c3bc8d3c6ccf27bf4e22420a75278fc73
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-31 13:44:21 +0530

    User Authorization Backend Routes (#3)
    * mongodb setup
    
    * routes indexing setup
    
    * write user schema
    
    * create /api/auth/register route
    
    * create /api/auth/login route
    
    * update credentials

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+
+import connectDB from './config/db.js';
+import router from './Routes/index.js';
 
 dotenv.config();
 
 const server = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 server.use(express.json());
+server.use(cors());
+
+// Connect to Database
+connectDB();
 
 // Routes
-server.get('/', (req, res) => res.send('Server is Ready'));
+server.get('/', (req, res) => res.send('Server is ready'));
+server.use("/api", router);
 
-server.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
