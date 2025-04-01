73bc41bf21092ec91fd449b5ac75d465e20cfcdc
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-04-01 10:17:28 +0000

    typo-fixes

--- frontend/src/components/footer.component.jsx
+++ frontend/src/components/footer.component.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="w-full text-gray-500 text-center py-4 my-4">
             <hr className="border-gray-300 my-4" />
-            <p>Copyright &copy; {year}, All Right Reserved</p>
+            <p>Copyright &copy; {year}, All Rights Reserved</p>
         </footer>
     );
 }
