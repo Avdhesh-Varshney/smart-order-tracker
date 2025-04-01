ef17b5fb2f9367ee6a885758f72c048874294f64
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-04-01 11:03:05 +0530

    Auth integration (#4)
    * setup register/login integration
    
    * setup session storage on page loading

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { Toaster, toast } from "react-hot-toast";
+
+import { storeInSession } from "../common/session";
+
 import InputBox from "../components/inputBox.component";
-import { Link } from "react-router-dom";
+
+import { UserContext } from "../App";
 
 const UserAuthForm = ({ type }) => {
+
+    let { userAuth: { access_token }, setUserAuth } = useContext(UserContext);
 
     let [selectedRole, setSelectedRole] = useState("user");
 
@@ -11,9 +20,76 @@ const UserAuthForm = ({ type }) => {
         setSelectedRole(e.target.value);
     }
 
+    const userAuthThroughServer = async (serverRoute, formData) => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
+            .then(({ data }) => {
+                storeInSession("user", JSON.stringify(data));
+                toast.success("Logged in successfully");
+
+                setTimeout(() => {
+                    setUserAuth(data);
+                }, 1000);
+            })
+            .catch(({ response }) => {
+                toast.error(response.data.error);
+            })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let serverRoute = type === "login" ? "/api/auth/login" : "/api/auth/register";
+
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+        let mobileRegex = /^[6-9]\d{9}$/;
+
+        let form = new FormData(formElement);
+        let formData = {};
+
+        for (let [key, value] of form.entries()) {
+            formData[key] = value;
+        }
+
+        let { fullname, role, adminKey, mobile, password } = formData;
+
+        if (type !== 'login') {
+            if (fullname.length < 3) {
+                return toast.error("Full name must be at least 3 characters long");
+            }
+
+            if (role === "admin") {
+                if (!adminKey) {
+                    return toast.error("Admin key is required");
+                }
+
+                if (adminKey !== import.meta.env.VITE_SECRET_ADMIN_KEY) {
+                    return toast.error("Invalid admin key");
+                }
+            }
+        }
+
+        if (mobile.length !== 10) {
+            return toast.error("Mobile number must be 10 digits long");
+        }
+
+        if (!mobileRegex.test(mobile)) {
+            return toast.error("Enter a valid mobile number");
+        }
+
+        if (!passwordRegex.test(password)) {
+            return toast.error("Password must be 8-20 characters long and contain at least one uppercase letter, one lowercase letter, and one number");
+        }
+
+        userAuthThroughServer(serverRoute, formData);
+    }
+
+    if (access_token) return <Navigate to="/" />;
+
     return (
         <section className="py-4 px-[5vw] md:px-[7vw] lg:px-[10vw] min-h-screen flex items-center justify-center">
-            <form className="w-[80%] max-w-[400px]">
+            <Toaster />
+
+            <form className="w-[80%] max-w-[400px]" id="formElement" onSubmit={handleSubmit}>
                 <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
                     {type === 'login' ? 'Welcome back' : 'Join us today'}
                 </h1>
