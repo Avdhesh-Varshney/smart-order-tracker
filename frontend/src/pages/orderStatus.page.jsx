f399e24177cac16334da4abec5b42c77e8e08782
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-04-01 08:28:31 +0000

    setup UI and structure of order status page

--- frontend/src/pages/orderStatus.page.jsx
+++ frontend/src/pages/orderStatus.page.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import AnimationWrapper from "../common/page-animation";
+import { useParams } from "react-router-dom";
+import NoDataMessage from "../components/noDataMessage.component";
+
 const OrderStatus = () => {
+
+    const {orderId} = useParams();
+
+    let [orderDetails, setOrderDetails] = useState(null);
     return (
-        <div>
-        <h1>Order Status</h1>
-        <p>Your order is being processed.</p>
-        </div>
+        <AnimationWrapper>
+            {
+                orderDetails ? (
+                    <div>
+                        Displaying the order details for order ID: {orderId}
+                    </div>
+                ) : (
+                    <NoDataMessage message={"No order details to display!"} />
+                )
+            }
+        </AnimationWrapper>
     );
 }
 
